2423d6dec77eb433d08afffc0737b729ca086489
Arthur Taouil <arthurtaouil@icloud.com>
2023-07-31 10:47:36 -0300

    Projeto criado e mensagem inicial para o usuario pronto

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,9 @@
-print("Olá Mundo!");
+print("Olá Mundo!")
 
-print("Este é um projeto inicial de Python.");
+print("Este é um projeto inicial de Python.")
+
+num1 = 10
+num2 = 30
+soma = num1 + num2
+
+print (soma)
